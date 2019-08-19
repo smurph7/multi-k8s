ec6886914f246f99c0b8b9fef0d83abc9c005fc9
smurph7 <sarahmurphy@Sarahs-MacBook-Pro.local>
2019-08-19 16:53:44 +1000

    update deploy.sh as GC runs on k8s version below1.15 - rollout restart command does not work

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t smurphy7/multi-client -f ./client/Dockerfile ./client
-docker build -t smurphy7/multi-server -f ./server/Dockerfile ./server
-docker build -t smurphy7/multi-worker -f ./worker/Dockerfile ./worker
-docker push smurphy7/multi-client
-docker push smurphy7/multi-server
-docker push smurphy7/multi-worker
+docker build -t smurphy7/multi-client:latest -t smurphy7/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t smurphy7/multi-server:latest -t smurphy7/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t smurphy7/multi-worker:latest -t smurphy7/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push smurphy7/multi-client:latest
+docker push smurphy7/multi-server:latest
+docker push smurphy7/multi-worker:latest
+
+docker push smurphy7/multi-client:$SHA
+docker push smurphy7/multi-server:$SHA
+docker push smurphy7/multi-worker:$SHA
+
 kubectl apply -f k8s
-kubectl rollout restart deployment client-deployment
-kubectl rollout restart deployment server-deployment
-kubectl rollout restart deployment worker-deployment
+kubectl set image deployments/client-deployment client=smurphy7/multi-client:$SHA
+kubectl set image deployments/server-deployment server=smurphy7/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=smurphy7/multi-worker:$SHA
